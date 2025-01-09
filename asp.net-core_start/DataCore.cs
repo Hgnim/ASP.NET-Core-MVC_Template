19133b310168da858595e05ed803a0a2bf38fc7a
Hgnim <hgn1m@outlook.com>
2025-01-09 22:55:58 +0800

    模板代码基本完成

---
+++ asp.net-core_start/DataCore.cs
@@ -0,0 +1,87 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace asp.net_core_start {
+	internal struct DataCore {
+		internal struct DataFiles {
+			static internal DataFile.ConfigFile config;
+		}
+	}
+	public struct DataFile {
+		/// <summary>
+		/// 将配置数据保存至配置文件中
+		/// </summary>
+		internal static void SaveData() {
+			ISerializer yamlS = new SerializerBuilder()
+				.WithNamingConvention(CamelCaseNamingConvention.Instance)
+				.Build();
+			File.WriteAllText(FilePath.configFile, yamlS.Serialize(DataCore.DataFiles.config));
+		}
+		/// <summary>
+		/// 读取数据文件并将数据写入实例中
+		/// </summary>
+		internal static void ReadData() {
+			IDeserializer yamlD = new DeserializerBuilder()
+				.WithNamingConvention(CamelCaseNamingConvention.Instance)
+					.Build();
+
+			DataCore.DataFiles.config = yamlD.Deserialize<ConfigFile>(File.ReadAllText(FilePath.configFile));
+		}
+		public struct ConfigFile {
+			public struct WebsiteModel {
+				public required string Addr { get; set; }
+				private string urlRoot;
+				public required string UrlRoot {
+					readonly get => urlRoot;
+					//对UrlRoot的值进行格式化
+					set {
+						string urlRoot_;
+						if (value == "/") urlRoot_ = "";//urlRoot不能只包含单独的斜杠
+						else if (value == "") { urlRoot_ = value; }//如果为空则直接输出
+						else {
+							if (value[..1] == "/")
+								urlRoot_ = value;
+							else//如果开头没有斜杠则加上斜杠
+								urlRoot_ = "/" + value;
+							if (urlRoot_.Substring(urlRoot_.Length - 1, 1) == "/")//如果末尾包含斜杠则去掉
+								urlRoot_ = urlRoot_[..(urlRoot_.Length - 1)];
+						}
+						urlRoot = urlRoot_;
+					}
+				}
+				
+				public required int Port { get; set; }
+				public required bool UseHttps { get; set; }
+				public required bool UseXFFRequestHeader { get; set; }
+				/* UseXFFRequestHeader
+				 * 后续用于获取客户端IP时可能会用到的选项
+				 string GetClientIP() {
+					if (Config.UseXFFRequestHeader)
+						return Request.Headers["X-Forwarded-For"].ToString();
+					else
+						return HttpContext.Connection.RemoteIpAddress!.ToString();
+				 }
+				 */
+			}
+			public required WebsiteModel Website { get; set; }
+			public required int DebugOutput { get; set; }
+			public required bool UpdateConfig { get; set; }
+
+			public ConfigFile() {
+				Website = new() {
+					Addr = "*",
+					UrlRoot = "/",
+					Port = 80,
+					UseHttps = false,
+					UseXFFRequestHeader = false
+				};
+				DebugOutput = 0;
+				UpdateConfig = false;
+			}
+		}
+	}
+	internal struct FilePath {
+		internal const string dataDir = "xxx_data/";//此处为服务端的数据文件目录
+		internal const string configFile = dataDir + "config.yml";
+	}
+}
---
+++ asp.net-core_start/Program.cs
@@ -0,0 +1,55 @@
+using static asp.net_core_start.DataCore;
+
+namespace asp.net_core_start
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			if (!Directory.Exists(FilePath.dataDir)) Directory.CreateDirectory(FilePath.dataDir);
+
+			if (File.Exists(FilePath.configFile)) {
+				try {
+					DataFile.ReadData();
+				} catch (Exception ex) { Console.WriteLine($"处理配置文件时出现错误，原因: {ex.Message}"); return; }
+
+				if (DataCore.DataFiles.config.UpdateConfig == true) {
+					DataCore.DataFiles.config.UpdateConfig = false;
+					DataFile.SaveData();
+					Console.WriteLine("配置文件已更新，已退出服务端");
+					return;
+				}
+			}
+			else {
+				DataFile.SaveData();
+			}
+
+
+			var builder = WebApplication.CreateBuilder(args);
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.WebHost.UseUrls($"{(DataFiles.config.Website.UseHttps?"https":"http")}://{DataFiles.config.Website.Addr}:{DataFiles.config.Website.Port}");
+			var app = builder.Build();
+			app.UsePathBase(DataFiles.config.Website.UrlRoot);
+			/*// Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }*/
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=InputMsg}/{action=Index}"); // /{id?}");
+
+			app.Run();
+		}
+    }
+}
